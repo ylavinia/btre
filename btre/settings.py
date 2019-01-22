6fd51a579d22608cd4bb90a692509cacb602c4cb
Yukhe Lavinia <bluishgrayfin@gmail.com>
2019-01-22 14:43:43 -0800

    Added local_settings import

--- btre/settings.py
+++ btre/settings.py
@@ -149,3 +149,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'aquapolar217@gmail.com'
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
